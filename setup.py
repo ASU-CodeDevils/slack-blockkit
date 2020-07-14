4306b87bc3f6a49b34c075e7a404c5b4db02ca92
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-07-14 01:47:54 -0400

    Modifications for version 0.0.2

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup
 
-import slack_blockkit
 
-
-def readme(file: str):
-    with open(file) as f:
-        return f.read()
-
-
-setup(
-    version=slack_blockkit.__version__,
-    long_description=readme("README.rst")
-)
+setup()
--- slack_blockkit/__init__.py
+++ slack_blockkit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1dev"
+__version__ = "0.0.2"
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num
