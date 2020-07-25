4b0ed056d7087fed81a87c3f3841384e37f02133
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-07-24 23:41:50 -0400

    Doc strings update using google styling

--- slack_blockkit/utils/blocks.py
+++ slack_blockkit/utils/blocks.py
@@ -8,10 +8,10 @@ def get_checkmark(task_completed: bool) -> str:
     Returns a check mark emoji indicating the completed task status. If the task is complete, then a white check
     mark is returned. If not, an empty white square is.
 
-    :param task_completed: Whether or not the task was complete.
-    :type task_completed: bool
-    :return: A checkmark emoji string based on whether or not the task was completed.
-    :rtype: str
+    Args:
+        task_completed (bool): Whether or not the task was complete.
+    Returns:
+        str: A checkmark emoji string based on whether or not the task was completed.
     """
     return ":white_check_mark:" if task_completed else ":white_large_square:"
 
@@ -20,29 +20,25 @@ def get_information_block(link: str, text: str) -> dict:
     """
     Returns an information block, which is a section with an info icon followed by linked text.
 
-    :param link: The link the block redirects the user to.
-    :type link: str
-    :param text: The link text.
-    :type text: str
-    :return: A dict in the format of a context block.
-    :rtype: dict
+    Args:
+        link (str): The link the block redirects the user to.
+        text (str): The link text.
+    Returns:
+        dict: A dict in the format of a context block.
     """
     information = ":information_source: *<{link}|{text}>*".format(link=link, text=text)
     return MarkdownTextObject(text=information).render()
 
 
-def get_text_block_with_accessory(
-    text_object: TextObject, accessory: BlockElement
-) -> dict:
+def get_text_block_with_accessory(text_object: TextObject, accessory: BlockElement) -> dict:
     """
     Returns a text block with an accessory.
 
-    :param text_object: The text block object.
-    :type text_object: TextObject
-    :param accessory: The accessory object.
-    :type accessory: LayoutBlock
-    :return: The text block with an accessory layout block.
-    :rtype: dict
+    Args:
+        text_object (TextObject): The text block object.
+        accessory (LayoutBlock): The accessory object.
+    Returns:
+        dict: The text block with an accessory layout block.
     """
     return SectionBlock(text=text_object, accessory=accessory).render()
 
@@ -51,14 +47,12 @@ def get_text_block_with_image(text: str, image_url: str, alt_text: str) -> dict:
     """
     Returns a text block with an image to the right of it.
 
-    :param text: The text in the text block.
-    :type text: str
-    :param image_url: The URL to the image.
-    :type image_url: str
-    :param alt_text: Alternate text (appears on image hover).
-    :type alt_text: str
-    :return: The block as a dict.
-    :rtype: dict
+    Args:
+        text (str): The text in the text block.
+        image_url (str): The URL to the image.
+        alt_text (str): Alternate text (appears on image hover).
+    Returns:
+        dict: The block as a dict.
     """
     text_object = MarkdownTextObject(text=text)
     image_element = ImageElement(image_url=image_url, alt_text=alt_text)
@@ -71,14 +65,12 @@ def get_task_block(text: str, info_link: str, info_text: str) -> list:
     """
     Returns a task block, which is comprised of a paragraph of text followed by an information link at the bottom.
 
-    :param text: Markdown-supported text to display in the paragraph.
-    :type text: str
-    :param info_link: The link associated with the task block.
-    :type info_link: str
-    :param info_text: The link text.
-    :type info_text: str
-    :return: An array of blocks formatted for a block payload.
-    :rtype: list
+    Args:
+        text (str): Markdown-supported text to display in the paragraph.
+        info_link (str): The link associated with the task block.
+        info_text (str): The link text.
+    Returns:
+        list: An array of blocks formatted for a block payload.
     """
     return [
         MarkdownTextObject(text=text).render(),
